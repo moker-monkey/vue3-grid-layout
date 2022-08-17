a043f89ca167c87fc4b8334016f21b948d5cacc1
肖昌铭 <xiaochangming@xiaochangmingdeMacBook-Pro.local>
2022-08-17 09:28:42 +0800

    [update]changeName

--- src/components/index.js
+++ src/components/index.js
@@ -8,25 +8,6 @@ const VueGridLayout = {
     GridItem
 }
 
-function install(Vue) {
-    if (install.installed) return;
-    install.installed = true;
-    Object.keys(VueGridLayout).forEach(name => {
-        Vue.component(name, VueGridLayout[name]);
-    });
-}
-VueGridLayout.install = install
-
-let GlobalVue = null;
-if (typeof window !== 'undefined') {
-    GlobalVue = window.Vue;
-} else if (typeof global !== 'undefined') {
-    GlobalVue = global.Vue;
-}
-if (GlobalVue) {
-    GlobalVue.use(VueGridLayout.install);
-}
-
 export default VueGridLayout;
 export {
     GridLayout,
